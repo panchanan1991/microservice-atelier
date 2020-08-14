a3f1c345692f97991ef24f0ee3458443cae10a19
panchanan1991@gmail.com <panchanan1991@gmail.com>
2020-08-14 12:07:51 +0530

    few update

--- springboot-configure-service/src/main/java/io/start/springbootconfigure/springbootconfigureservice/SpringbootConfigureServiceApplication.java
+++ springboot-configure-service/src/main/java/io/start/springbootconfigure/springbootconfigureservice/SpringbootConfigureServiceApplication.java
@@ -2,13 +2,22 @@ package io.start.springbootconfigure.springbootconfigureservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author panchanan
+ * This class is the starting of productDetailsService
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringbootConfigureServiceApplication {
-
+	
+	/**
+	 * SpirngBoot Main method
+	 * @param args
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootConfigureServiceApplication.class, args);
 	}
