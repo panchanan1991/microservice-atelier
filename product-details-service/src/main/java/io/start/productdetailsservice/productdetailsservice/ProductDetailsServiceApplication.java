69d095287bd7d94283293127faaed2ced2108810
panchanan1991@gmail.com <panchanan1991@gmail.com>
2020-08-14 12:13:30 +0530

    few update

--- product-details-service/src/main/java/io/start/productdetailsservice/productdetailsservice/ProductDetailsServiceApplication.java
+++ product-details-service/src/main/java/io/start/productdetailsservice/productdetailsservice/ProductDetailsServiceApplication.java
@@ -3,6 +3,7 @@ package io.start.productdetailsservice.productdetailsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
--- product-details-service/src/main/java/io/start/productdetailsservice/productdetailsservice/service/ProductDetailsService.java
+++ product-details-service/src/main/java/io/start/productdetailsservice/productdetailsservice/service/ProductDetailsService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,7 @@ import io.start.productdetailsservice.productdetailsservice.model.ProductDetails
  *
  */
 @Service
+@RefreshScope
 public class ProductDetailsService {
 	
 	@Value("${api.key}")
