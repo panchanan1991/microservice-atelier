06468111c90b6591bb51e9992b7d08effec0c749
panchanan1991@gmail.com <panchanan1991@gmail.com>
2020-08-14 22:57:33 +0530

    some minor changes

--- discovery-server/src/main/java/io/start/discoveryserver/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/start/discoveryserver/discoveryserver/DiscoveryServerApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author panchanan
+ * This class is the starting of DiscoveryServerApplication
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
 
+	/**
+	 * Main method of Spring boot application service
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
